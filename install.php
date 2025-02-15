674ba0f3cccab90b89f875c408e20d07e933a309
BeefSnot <161774551+BeefSnot@users.noreply.github.com>
2025-02-14 21:21:58 -0600

    test

--- install.php
+++ install.php
@@ -112,7 +112,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Install Newsletter Software</title>
-    <link rel="stylesheet" href="assets/css/newsletter.css">
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
     <main>
